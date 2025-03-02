dc43ef2aa9675749ecaef36ca1250109dd47dccd
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-03-02 10:18:57 +0545

    category

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.app')
+@section('title', 'Dashboard')
+@section('content')
+    <div class="grid grid-cols-3 gap-4 ">
+        <div class="bg-blue-100 px-4 py-8 rounded-lg shadow flex items-center justify-between">
+            <h2 class="text-lg">Total Products</h2>
+            <span class="text-4xl font-bold">100</span>
+        </div>
+
+        <div class="bg-red-100 px-4 py-8 rounded-lg shadow flex items-center justify-between">
+            <h2 class="text-lg">Pending Orders</h2>
+            <span class="text-4xl font-bold">250</span>
+        </div>
+
+        <div class="bg-green-100 px-4 py-8 rounded-lg shadow flex items-center justify-between">
+            <h2 class="text-lg">Total Orders</h2>
+            <span class="text-4xl font-bold">360</span>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,21 @@
         <div class="flex">
             <div class="w-56 h-screen bg-gray-100 shadow-lg border border-gray-200">
                 <img src="https://picsum.photos/500" alt="" class="w-1/2 mx-auto py-5">
-                <div class="mt-4">
-                    <a href="" class="text-xl font-bold">Dashboard</a>
+                <div class="mt-4 grid">
+                    <a href="{{route('dashboard')}}" class="text-xl font-bold p-4 border-b hover:bg-gray-300">Dashboard</a>
+                    <a href="" class="text-xl font-bold p-4 border-b hover:bg-gray-300">Categories</a>
+                    <a href="" class="text-xl font-bold p-4 border-b hover:bg-gray-300">Products</a>
+                    <a href="" class="text-xl font-bold p-4 border-b hover:bg-gray-300">Orders</a>
+                    <a href="" class="text-xl font-bold p-4 border-b hover:bg-gray-300">Users</a>
+                    <a href="" class="text-xl font-bold p-4 border-b hover:bg-gray-300">Logout</a>
                 </div>
             </div>
             <div class="p-4 flex-1">
-                <h2 class="text-3xl font-bold">Dashboard</h2>
+                <h2 class="text-3xl font-bold">@yield('title')</h2>
                 <hr class="h-1 bg-red-600">
+                <div class="mt-4">
+                    @yield('content')
+                </div>
             </div>
         </div>
     </body>
