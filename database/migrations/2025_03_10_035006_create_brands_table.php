60abaee61f5a6bc9e72a3760d1ff1a9f9a0ce4b0
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-03-11 10:02:06 +0545

    updates

--- database/migrations/2025_03_10_035006_create_brands_table.php
+++ database/migrations/2025_03_10_035006_create_brands_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('priority');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
@@ -22,6 +23,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/category/{id}/edit',[CategoryController::class,'edit'])->name('category.edit');
     Route::post('/category/{id}/update',[CategoryController::class,'update'])->name('category.update');
     Route::post('/category/destroy',[CategoryController::class,'destroy'])->name('category.destroy');
+
+    //Brand
+    Route::get('/brand', [BrandController::class, 'index'])->name('brand.index');
+    Route::get('/brand/create',[BrandController::class,'create'])->name('brand.create');
+    Route::post('/brand/store',[BrandController::class,'store'])->name('brand.store');
+    Route::get('/brand/{id}/edit',[BrandController::class,'edit'])->name('brand.edit');
+    Route::post('/brand/{id}/update',[BrandController::class,'update'])->name('brand.update');
+    Route::post('/brand/destroy',[BrandController::class,'destroy'])->name('brand.destroy');
 
     //Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
