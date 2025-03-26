d94cc3a758ea21820860c7fe7595dffd48b99695
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-03-26 10:10:35 +0545

    updats

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $latestproducts = Product::latest()->take(4)->get();
+        return view('welcome', compact('latestproducts'));
     }
 
     public function about()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.master')
 @section('content')
     <div class="py-10 px-20 bg-gray-100">
-        <h1 class="text-4xl font-bold">Welcome Here</h1>
-        <p class="text-lg mt-4">Vite is a next-generation frontend tooling that sets you up for success. It's fast, it's reliable and it's easy to use.</p>
+        <h1 class="text-4xl font-bold">Latest Products</h1>
+
+        <div class="grid grid-cols-4 gap-5 mt-5">
+            @foreach($latestproducts as $product)
+            <a href="" class="shadow-md p-2 border">
+                <img src="{{asset('images/products/'.$product->photopath)}}" alt="product" class="w-full h-52 object-cover">
+                <div class="mt-2">
+                    <h1 class="text-xl font-bold">{{$product->name}}</h1>
+                    @if($product->discounted_price > 0)
+                    <p class="text-md font-bold">Rs. {{$product->discounted_price}} <span class="line-through font-normal text-sm">Rs. {{$product->price}}</span> </p>
+                    @else
+                    <p class="text-md font-bold">Rs. {{$product->price}}</p>
+                    @endif
+                </div>
+            </a>
+            @endforeach
+
+        </div>
+
     </div>
 @endsection
